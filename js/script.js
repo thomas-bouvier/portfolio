6b7e99b99ba67eac700b92f84a856e09282007ce
Thomas Bouvier <tomatrocho@gmail.com>
2018-10-10 23:51:26 +0200

    Added projects grid

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+/*
 var containerEl = document.querySelector('.min-container');
 var mixer = mixitup(containerEl);
+*/
 
 particlesJS.load('particles-js', './js/particles/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
