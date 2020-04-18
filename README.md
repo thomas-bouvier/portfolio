43e94ee47f1debc949eee7bdb166b4498e9025a1
Thomas Bouvier <contact@thomas-bouvier.io>
2020-04-19 00:20:40 +0200

    Delete particle effect on hero images

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # portfolio
+
+Don't forget to add `files/resume_thomas_bouvier.pdf` and `files/cv_thomas_bouvier.pdf` on the server.
--- js/script.js
+++
@@ -1,8 +0,0 @@
-/*
-var containerEl = document.querySelector('.min-container');
-var mixer = mixitup(containerEl);
-*/
-
-particlesJS.load('particles-js', './js/particles/particlesjs-config.json', function() {
-  console.log('callback - particles.js config loaded');
-});
